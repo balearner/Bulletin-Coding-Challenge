ceba228c5ef223702288cec0eb5c8185717b1639
thusxy00 <96934492+balearner@users.noreply.github.com>
2023-10-06 17:20:49 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,23 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+# create the user
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+
+
+# create the note of the user
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+# create a new house
